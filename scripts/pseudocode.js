ac1fb9777149e4b02a35c631f8f130249252eae1
wommy <tom@technomad.media>
2020-10-22 21:48:41 -0400

    pushit

--- scripts/pseudocode.js
+++ scripts/pseudocode.js
@@ -9,8 +9,14 @@
 // add new material
 // takes a name
 
+// wont work
+// 
 // upon new material
 // new quantity = 0
+//
+// need to init a quantity of 0 upon new materials
+//
+// new material => quantity( 0 )
 
 // list materials
 
