6758c600f86b149db359896238222c8c48ecdf12
wommy <tom@technomad.media>
2020-10-22 21:47:14 -0400

    pushit

---
+++ scripts/pseudocode.js
@@ -0,0 +1,19 @@
+
+// what am i doing
+
+// new array Materials
+
+// list materials
+// console.log( materials )
+
+// add new material
+// takes a name
+
+// upon new material
+// new quantity = 0
+
+// list materials
+
+// 
+
+// add material
